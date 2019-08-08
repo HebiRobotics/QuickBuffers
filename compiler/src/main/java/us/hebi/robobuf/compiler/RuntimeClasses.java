4a226bdc9e18c58a09f499625dc30a518a68fc9b
Florian Enner <florian@hebirobotics.com>
2019-08-08 22:06:18 +0200

    added mergeFrom outline with wire dependency

--- compiler/src/main/java/us/hebi/robobuf/compiler/RuntimeClasses.java
+++ compiler/src/main/java/us/hebi/robobuf/compiler/RuntimeClasses.java
@@ -1,9 +1,13 @@
 package us.hebi.robobuf.compiler;
 
+import com.squareup.javapoet.ClassName;
+
 /**
  * @author Florian Enner
  * @since 07 Aug 2019
  */
 public class RuntimeClasses {
+
+    public static final ClassName PROTO_SOURCE = ClassName.get("com.squareup.wire", "ProtoReader");
 
 }
--- compiler/src/main/java/us/hebi/robobuf/compiler/type/MessageGenerator.java
+++ compiler/src/main/java/us/hebi/robobuf/compiler/type/MessageGenerator.java
@@ -4,12 +4,14 @@ import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
-import us.hebi.robobuf.compiler.RequestInfo.MessageInfo;
 import us.hebi.robobuf.compiler.BitField;
+import us.hebi.robobuf.compiler.RequestInfo.MessageInfo;
+import us.hebi.robobuf.compiler.RuntimeClasses;
 import us.hebi.robobuf.compiler.field.FieldGenerator;
 import us.hebi.robobuf.compiler.field.FieldGenerators;
 
 import javax.lang.model.element.Modifier;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +23,9 @@ public class MessageGenerator implements TypeGenerator {
 
     public MessageGenerator(MessageInfo info) {
         this.info = info;
-        info.getNestedTypes().forEach(t -> nestedTypes.add(new MessageGenerator(t)));
-        info.getNestedEnums().forEach(t -> nestedTypes.add(new EnumGenerator(t)));
         info.getFields().forEach(f -> fields.add(FieldGenerators.createGenerator(f)));
+        info.getNestedEnums().forEach(t -> nestedTypes.add(new EnumGenerator(t)));
+        info.getNestedTypes().forEach(t -> nestedTypes.add(new MessageGenerator(t)));
     }
 
     public TypeSpec generate() {
@@ -44,26 +46,29 @@ public class MessageGenerator implements TypeGenerator {
                     .build());
         }
 
-        // Types
-        nestedTypes.stream()
-                .map(TypeGenerator::generate)
-                .forEach(type::addType);
-
-        // Fields
-        fields.forEach(f-> f.generateMembers(type));
-        generateCopyFrom(type);
-        generateClear(type);
-
         // Has-state
         for (int i = 0; i < BitField.getNumberOfFields(info.getFieldCount()); i++) {
             type.addField(FieldSpec.builder(int.class, BitField.fieldName(i), Modifier.PRIVATE).build());
         }
 
-        // Internal methods
+        // Fields
+        fields.forEach(f -> f.generateMembers(type));
+        generateCopyFrom(type);
+        generateClear(type);
+
+        // Internal Constructor
         type.addMethod(MethodSpec.constructorBuilder()
                 .addModifiers(Modifier.PUBLIC)
                 .addStatement("clear()")
                 .build());
+
+        generateMergeFrom(type);
+
+        // Types
+        nestedTypes.stream()
+                .map(TypeGenerator::generate)
+                .forEach(type::addType);
+
         return type.build();
     }
 
@@ -71,7 +76,9 @@ public class MessageGenerator implements TypeGenerator {
         MethodSpec.Builder clear = MethodSpec.methodBuilder("clear")
                 .addModifiers(Modifier.PUBLIC)
                 .returns(info.getTypeName());
-        // TODO: clear bitfields
+        for (int i = 0; i < BitField.getNumberOfFields(fields.size()); i++) {
+            clear.addStatement("$L = 0", BitField.fieldName(0));
+        }
         fields.forEach(field -> field.generateClearCode(clear));
         clear.addStatement("return this");
         type.addMethod(clear.build());
@@ -89,15 +96,36 @@ public class MessageGenerator implements TypeGenerator {
 
     }
 
+    private void generateMergeFrom(TypeSpec.Builder type) {
+        MethodSpec.Builder mergeFrom = MethodSpec.methodBuilder("mergeFrom")
+                .addModifiers(Modifier.PUBLIC)
+                .returns(info.getTypeName())
+                .addParameter(RuntimeClasses.PROTO_SOURCE, "input")
+                .addException(IOException.class);
+        mergeFrom.beginControlFlow("while (true)")
+                .addStatement("int tag = input.nextTag()")
+                .beginControlFlow("switch (tag)")
+                .beginControlFlow("case 0:")
+                .addStatement("return this")
+                .endControlFlow()
+                .beginControlFlow("default:")
+                .addStatement("break")
+                .endControlFlow()
+                .endControlFlow()
+                .endControlFlow();
+        mergeFrom.addStatement("return this");
+        type.addMethod(mergeFrom.build());
+    }
+
     private void generateCopyFrom(TypeSpec.Builder type) {
         MethodSpec.Builder copyFrom = MethodSpec.methodBuilder("copyFrom")
                 .addParameter(info.getTypeName(), "other", Modifier.FINAL)
                 .addModifiers(Modifier.PUBLIC)
                 .returns(info.getTypeName());
-        fields.forEach(field -> field.generateCopyFromCode(copyFrom));
         for (int i = 0; i < BitField.getNumberOfFields(fields.size()); i++) {
             copyFrom.addStatement("$1L = other.$1L", BitField.fieldName(i));
         }
+        fields.forEach(field -> field.generateCopyFromCode(copyFrom));
         copyFrom.addStatement("return this"); // TODO: remember dirty bit
         type.addMethod(copyFrom.build());
     }
