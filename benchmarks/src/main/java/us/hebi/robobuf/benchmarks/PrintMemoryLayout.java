19d88d95acf3b23cd3da00a6fe19bd5fe06fa5c1
Florian Enner <florian@hebirobotics.com>
2019-10-15 00:08:06 +0200

    extended memory layout check

--- benchmarks/src/main/java/us/hebi/robobuf/benchmarks/PrintMemoryLayout.java
+++ benchmarks/src/main/java/us/hebi/robobuf/benchmarks/PrintMemoryLayout.java
@@ -2,7 +2,13 @@ package us.hebi.robobuf.benchmarks;
 
 import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.info.FieldLayout;
+import org.openjdk.jol.info.GraphLayout;
+import org.openjdk.jol.vm.VM;
+import org.openjdk.jol.vm.VirtualMachine;
+import us.hebi.robobuf.ProtoMessage;
+import us.hebi.robobuf.robo.TestAllTypes;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -16,7 +22,7 @@ import java.util.stream.Stream;
  */
 public class PrintMemoryLayout {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, IOException {
         Class<?> clazz = us.hebi.robobuf.robo.TestAllTypes.class;
         ClassLayout layout = ClassLayout.parseClass(clazz);
         FieldOffsetMap offsetMap = new FieldOffsetMap(layout);
@@ -31,9 +37,45 @@ public class PrintMemoryLayout {
         for (Field field : fields) {
             long offset = offsetMap.getOffset(field);
             long cacheLine = offset / CACHE_LINE_SIZE; // assumes alignment with cache line boundary, which may not be true
-            System.out.println(String.format("[%3d => %3d (%1d)] %s", (i++), offset, cacheLine, field.getName()));
+            String string = String.format("[%3d => %3d (%1d)] %s", (i++), offset, cacheLine, field.getName());
+            System.out.println(string);
         }
 
+        System.out.println("---- Object Graph ---- ");
+        ProtoMessage msg = new TestAllTypes();
+        System.out.println(GraphLayout.parseInstance(msg).toPrintable());
+
+        System.out.println("---- Offsets of Nested Messages ---- ");
+        printNestedOffsets(msg);
+
+    }
+
+    private static void printNestedOffsets(ProtoMessage message) throws IllegalAccessException {
+        printNestedOffsets(null, message.getClass().getSimpleName(), message, 0);
+    }
+
+    /**
+     * running it causes allocations and mess with results. unreliable
+     */
+    private static void printNestedOffsets(ProtoMessage parent, String name, ProtoMessage message, int indents) throws IllegalAccessException {
+        for (int i = 0; i < indents; i++) {
+            System.out.print(" ");
+        }
+
+        long endOfParent = parent == null ? vm.addressOf(message) : vm.addressOf(parent) + vm.sizeOf(parent);
+        long start = vm.addressOf(message) - endOfParent;
+        long end = start + vm.sizeOf(message);
+        System.out.println(String.format("%d-%d : %s", start, end, name));
+
+        for (Field field : message.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+
+            Object value = field.get(message);
+            if (value instanceof ProtoMessage) {
+                printNestedOffsets(message, field.getName(), (ProtoMessage) value, indents + 2);
+            }
+
+        }
     }
 
     private static List<Field> getInstanceFields(Class<?> clazz) {
@@ -66,5 +108,6 @@ public class PrintMemoryLayout {
     }
 
     private static final int CACHE_LINE_SIZE = 64;
+    private static VirtualMachine vm = VM.current();
 
 }
