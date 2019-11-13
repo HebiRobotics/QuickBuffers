675468f2cf630890623d83152622e91267e8123e
Florian Enner <florian@hebirobotics.com>
2019-11-13 23:30:04 +0100

    ported NumberEncoding from JsonIter

--- runtime/src/main/java/us/hebi/robobuf/JsonUtil.java
+++ runtime/src/main/java/us/hebi/robobuf/JsonUtil.java
@@ -207,9 +207,6 @@ class JsonUtil {
 
     /**
      * Copied from JDK12's Long.toString() implementation
-     *
-     * TODO: write numbers 3 digits at once. see JsonIter / DSL-Platform?
-     * https://github.com/json-iterator/java/blob/master/src/main/java/com/jsoniter/output/StreamImplNumber.java
      */
     static class IntegerEncoding {
 
@@ -354,9 +351,6 @@ class JsonUtil {
 
     }
 
-    /**
-     * TODO: implement without allocations
-     */
     static class FloatEncoding {
 
         static void writeFloat(float value, RepeatedByte output) {
@@ -387,6 +381,261 @@ class JsonUtil {
         private static final byte[] NEGATIVE_INF = "\"-Infinity\"".getBytes(Charsets.ISO_8859_1);
         private static final byte[] POSITIVE_INF = "\"Infinity\"".getBytes(Charsets.ISO_8859_1);
         private static final byte[] NAN = "\"NaN\"".getBytes(Charsets.ISO_8859_1);
+
+    }
+
+    /**
+     * Copied from JsonIter / DSL-Platform
+     * https://github.com/json-iterator/java/blob/master/src/main/java/com/jsoniter/output/StreamImplNumber.java
+     */
+    static class NumberEncoding {
+
+        private final static int[] DIGITS = new int[1000];
+
+        static {
+            for (int i = 0; i < 1000; i++) {
+                DIGITS[i] = (i < 10 ? (2 << 24) : i < 100 ? (1 << 24) : 0)
+                        + (((i / 100) + '0') << 16)
+                        + ((((i / 10) % 10) + '0') << 8)
+                        + i % 10 + '0';
+            }
+        }
+
+        private static final byte[] MIN_INT = "-2147483648".getBytes();
+
+        public static final void writeInt(int value, final RepeatedByte output) {
+            output.reserve(12);
+            byte[] buf = output.array;
+            int pos = output.length;
+            if (value < 0) {
+                if (value == Integer.MIN_VALUE) {
+                    System.arraycopy(MIN_INT, 0, buf, pos, MIN_INT.length);
+                    output.length = pos + MIN_INT.length;
+                    return;
+                }
+                value = -value;
+                buf[pos++] = '-';
+            }
+            final int q1 = value / 1000;
+            if (q1 == 0) {
+                pos += writeFirstBuf(buf, DIGITS[value], pos);
+                output.length = pos;
+                return;
+            }
+            final int r1 = value - q1 * 1000;
+            final int q2 = q1 / 1000;
+            if (q2 == 0) {
+                final int v1 = DIGITS[r1];
+                final int v2 = DIGITS[q1];
+                int off = writeFirstBuf(buf, v2, pos);
+                writeBuf(buf, v1, pos + off);
+                output.length = pos + 3 + off;
+                return;
+            }
+            final int r2 = q1 - q2 * 1000;
+            final long q3 = q2 / 1000;
+            final int v1 = DIGITS[r1];
+            final int v2 = DIGITS[r2];
+            if (q3 == 0) {
+                pos += writeFirstBuf(buf, DIGITS[q2], pos);
+            } else {
+                final int r3 = (int) (q2 - q3 * 1000);
+                buf[pos++] = (byte) (q3 + '0');
+                writeBuf(buf, DIGITS[r3], pos);
+                pos += 3;
+            }
+            writeBuf(buf, v2, pos);
+            writeBuf(buf, v1, pos + 3);
+            output.length = pos + 6;
+        }
+
+        private static int writeFirstBuf(final byte[] buf, final int v, int pos) {
+            final int start = v >> 24;
+            if (start == 0) {
+                buf[pos++] = (byte) (v >> 16);
+                buf[pos++] = (byte) (v >> 8);
+            } else if (start == 1) {
+                buf[pos++] = (byte) (v >> 8);
+            }
+            buf[pos] = (byte) v;
+            return 3 - start;
+        }
+
+        private static void writeBuf(final byte[] buf, final int v, int pos) {
+            buf[pos] = (byte) (v >> 16);
+            buf[pos + 1] = (byte) (v >> 8);
+            buf[pos + 2] = (byte) v;
+        }
+
+        private static final byte[] MIN_LONG = "-9223372036854775808".getBytes();
+
+        public static final void writeLong(long value, final RepeatedByte output) {
+            output.reserve(22);
+            byte[] buf = output.array;
+            int pos = output.length;
+            if (value < 0) {
+                if (value == Long.MIN_VALUE) {
+                    System.arraycopy(MIN_LONG, 0, buf, pos, MIN_LONG.length);
+                    output.length = pos + MIN_LONG.length;
+                    return;
+                }
+                value = -value;
+                buf[pos++] = '-';
+            }
+            final long q1 = value / 1000;
+            if (q1 == 0) {
+                pos += writeFirstBuf(buf, DIGITS[(int) value], pos);
+                output.length = pos;
+                return;
+            }
+            final int r1 = (int) (value - q1 * 1000);
+            final long q2 = q1 / 1000;
+            if (q2 == 0) {
+                final int v1 = DIGITS[r1];
+                final int v2 = DIGITS[(int) q1];
+                int off = writeFirstBuf(buf, v2, pos);
+                writeBuf(buf, v1, pos + off);
+                output.length = pos + 3 + off;
+                return;
+            }
+            final int r2 = (int) (q1 - q2 * 1000);
+            final long q3 = q2 / 1000;
+            if (q3 == 0) {
+                final int v1 = DIGITS[r1];
+                final int v2 = DIGITS[r2];
+                final int v3 = DIGITS[(int) q2];
+                pos += writeFirstBuf(buf, v3, pos);
+                writeBuf(buf, v2, pos);
+                writeBuf(buf, v1, pos + 3);
+                output.length = pos + 6;
+                return;
+            }
+            final int r3 = (int) (q2 - q3 * 1000);
+            final int q4 = (int) (q3 / 1000);
+            if (q4 == 0) {
+                final int v1 = DIGITS[r1];
+                final int v2 = DIGITS[r2];
+                final int v3 = DIGITS[r3];
+                final int v4 = DIGITS[(int) q3];
+                pos += writeFirstBuf(buf, v4, pos);
+                writeBuf(buf, v3, pos);
+                writeBuf(buf, v2, pos + 3);
+                writeBuf(buf, v1, pos + 6);
+                output.length = pos + 9;
+                return;
+            }
+            final int r4 = (int) (q3 - q4 * 1000);
+            final int q5 = q4 / 1000;
+            if (q5 == 0) {
+                final int v1 = DIGITS[r1];
+                final int v2 = DIGITS[r2];
+                final int v3 = DIGITS[r3];
+                final int v4 = DIGITS[r4];
+                final int v5 = DIGITS[q4];
+                pos += writeFirstBuf(buf, v5, pos);
+                writeBuf(buf, v4, pos);
+                writeBuf(buf, v3, pos + 3);
+                writeBuf(buf, v2, pos + 6);
+                writeBuf(buf, v1, pos + 9);
+                output.length = pos + 12;
+                return;
+            }
+            final int r5 = q4 - q5 * 1000;
+            final int q6 = q5 / 1000;
+            final int v1 = DIGITS[r1];
+            final int v2 = DIGITS[r2];
+            final int v3 = DIGITS[r3];
+            final int v4 = DIGITS[r4];
+            final int v5 = DIGITS[r5];
+            if (q6 == 0) {
+                pos += writeFirstBuf(buf, DIGITS[q5], pos);
+            } else {
+                final int r6 = q5 - q6 * 1000;
+                buf[pos++] = (byte) (q6 + '0');
+                writeBuf(buf, DIGITS[r6], pos);
+                pos += 3;
+            }
+            writeBuf(buf, v5, pos);
+            writeBuf(buf, v4, pos + 3);
+            writeBuf(buf, v3, pos + 6);
+            writeBuf(buf, v2, pos + 9);
+            writeBuf(buf, v1, pos + 12);
+            output.length = pos + 15;
+        }
+
+        private static final int POW10[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+
+        public static final void writeFloat(float val, final RepeatedByte output) {
+            if (val < 0) {
+                if (val == Float.NEGATIVE_INFINITY) {
+                    output.addAll(FloatEncoding.NEGATIVE_INF);
+                    return;
+                }
+                output.add((byte) '-');
+                val = -val;
+            }
+            if (val > 0x4ffffff) {
+                if (val == Float.POSITIVE_INFINITY) {
+                    output.addAll(FloatEncoding.POSITIVE_INF);
+                    return;
+                }
+                StringEncoding.writeRawAscii(Float.toString(val), output);
+                return;
+            }
+            int precision = 6;
+            int exp = 1000000; // 6
+            long lval = (long) (val * exp + 0.5);
+            writeLong(lval / exp, output);
+            long fval = lval % exp;
+            if (fval == 0) {
+                return;
+            }
+            output.add((byte) '.');
+            output.reserve(11);
+            for (int p = precision - 1; p > 0 && fval < POW10[p]; p--) {
+                output.array[output.length++] = '0';
+            }
+            writeLong(fval, output);
+            while (output.array[output.length - 1] == '0') {
+                output.length--;
+            }
+        }
+
+        public static final void writeDouble(double val, final RepeatedByte output) {
+            if (val < 0) {
+                if (val == Double.NEGATIVE_INFINITY) {
+                    output.addAll(FloatEncoding.NEGATIVE_INF);
+                    return;
+                }
+                val = -val;
+                output.add((byte) '-');
+            }
+            if (val > 0x4ffffff) {
+                if (val == Double.POSITIVE_INFINITY) {
+                    output.addAll(FloatEncoding.POSITIVE_INF);
+                    return;
+                }
+                StringEncoding.writeRawAscii(Double.toString(val), output);
+                return;
+            }
+            int precision = 6;
+            int exp = 1000000; // 6
+            long lval = (long) (val * exp + 0.5);
+            writeLong(lval / exp, output);
+            long fval = lval % exp;
+            if (fval == 0) {
+                return;
+            }
+            output.add((byte) '.');
+            output.reserve(11);
+            for (int p = precision - 1; p > 0 && fval < POW10[p]; p--) {
+                output.array[output.length++] = '0';
+            }
+            writeLong(fval, output);
+            while (output.array[output.length - 1] == '0') {
+                output.length--;
+            }
+        }
 
     }
 
